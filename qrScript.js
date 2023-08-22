18c349cffae7f6f9d5e3ef3ef2a792dae4ccce78
mauriciomontivero <126199622+mauriciomontivero@users.noreply.github.com>
2023-08-22 14:35:27 -0300

    Add files via upload

--- qrScript.js
+++ qrScript.js
@@ -1,38 +1,3 @@
-/*! Aca arrancan los codigos del buscador */
-
-
-function doGet() {
-  return HtmlService.createTemplateFromFile('Index').evaluate();
-}
-
-
-/* PROCESS FORM */
-function processForm(formObject){  
-  var result = "";
-  if(formObject.searchtext){//Execute if form passes search text
-      result = search(formObject.searchtext);
-  }
-  return result;
-}
-
-//SEARCH FOR MATCHED CONTENTS 
-function search(searchtext){
-  var spreadsheetId   = '1OAKedtGR7ezGhQUnQJbfChZcsaOBjKVuMFQ3Q2vgr7k'; //** CHANGE !!!
-  var dataRage        = 'Articulos!A2:D';                                    //** CHANGE !!!
-  var data = Sheets.Spreadsheets.Values.get(spreadsheetId, dataRage).values;
-  var ar = [];
-  
-  data.forEach(function(f) {
-    if (~f.indexOf(searchtext)) {
-      ar.push(f);
-    }
-  });
-  return ar;
-}
-
-
-
-
 /*! Aca arrancan los codigos del escaner */
 
 /*! For license information please see html5-qrcode.min.js.LICENSE.txt */
